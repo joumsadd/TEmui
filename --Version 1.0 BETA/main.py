942ae588a616e19f3cedf2d6fa7851232e8c3977
Омар Ильсов <123903001+joumsadd@users.noreply.github.com>
2023-12-20 19:14:08 +0400

    Updated main.py
    Added new 'Sideload' command for emulating DebuXposed!
    Added support for 'cd' directory changing command
    Added some optimization and improved stability for info() on Windows 10,11!

--- --Version 1.0 BETA/main.py
+++ --Version 1.0 BETA/main.py
@@ -3,56 +3,75 @@ import time
 import subprocess as sbp
 import sys, os
 from colorama import Fore, Back, Style
-from tkinter import *
 from tkinter import messagebox as msg
 
 
+def sideload():
+    command = input('>>> ')
+    if command.lower() == '0':
+        debux()
+    elif command.lower().startswith('cd '):
+        # If the command is cd, change the current directory for sideload
+        try:
+            path = command[3:].strip()  # Get the path after "cd " and remove possible spaces
+            os.chdir(path)
+            print(f"Current directory for sideload: {os.getcwd()}")
+        except FileNotFoundError:
+            print(f"Directory '{path}' not found.")
+        except Exception as e:
+            print(f"Error changing directory: {e}")
+        sideload()
+    else:
+        sbp.run(command, shell=True)
+        print()
+        sideload()
+
 def debux():
     deb_command = input('--> ')
-    if deb_command.lower() == 'help':
+    if 'help' in deb_command.lower():
         print('Debug Xposed command list :')
         print('1. SIDELOAD - Starting cmd push commands(echo, exit,cls)')
         print('2. Abort status - Clearing debugging info txt')
         print('3. Debux Info - Showing DebuX version.')
         debux()
-    if deb_command.lower() == 'abort status /info/':
+    elif 'abort status /info/' in deb_command.lower():
         file_name = 'LocalDeb_Temp-USER.txt'
         with open(file_name, 'w') as file:
-            file.write('/cls/')
+            write = file.write('')
         with open(file_name, 'r') as file:
-            content = file.read()        
-        if 'cls' in content:
-            print(Fore.GREEN+'Debugging info has been successfully cleared!')
-            print(Style.RESET_ALL)
-            debux()
-    elif deb_command.lower() == 'sideload':
+            content = file.read()   
+        print(Fore.GREEN+'Debugging info has been successfully cleared!')
+        print(Style.RESET_ALL)
+        debux()
+    elif 'sideload' in deb_command.lower():
         print('Cmd Emulator Mode : ONN')
         print('Type 0 for exit')
         time.sleep(0.4)
-        def sideload():
-            command = input('>>> ')
-            if command == '0':
-                debux()
-            else:
-                pass
-            sbp.run(command, shell=True)
-            print()
-            sideload()
         sideload()
     elif 'debux' in deb_command.lower():
         print('DebuX v1.0.19045.3693 by Joumsadd')
                 
-    elif deb_command.lower() == 'exit':
+    elif 'exit' in deb_command.lower():
         yesno = msg.askyesno(title='Exiting DebuX', message='Do you want to close DebuX? ')
         if yesno == True:
             start()
         else:
             debux()
+    elif deb_command.lower().startswith('cd '):
+        # If the command is cd, change the current directory
+        try:
+            path = deb_command[3:].strip()  # Get the path after "cd " and remove possible spaces
+            os.chdir(path)
+            print(f"Current directory: {os.getcwd()}")
+        except FileNotFoundError:
+            print(f"Directory '{path}' not found.")
+        except Exception as e:
+            print(f"Error changing directory: {e}")
+        debux()
     else:
         print(Fore.RED+'An unexpected command!')
         print(Style.RESET_ALL)
         debux()
-
 
 def su():
     path = input('Enter file directory: ')
@@ -120,19 +139,20 @@ def start():
         print(Fore.RED+'Unknown Command!')
         print(Style.RESET_ALL)
 
-
 def debug_file(command):
     file_name = 'LocalDeb_Temp-USER.txt'
     now = time.strftime('%H:%M:%S', time.localtime())
     os_version = os.sys.getwindowsversion()
+
     with open(file_name, 'r') as file:
         content = file.read()
+
     if 'starting' in content.lower():
         with open(file_name, 'a') as file:
-            file.write(f'{now} Starting {command.upper()}-command for {time.time()}, OS Version :{os_version.major}.{os_version.minor}')
+            file.write(f'{now} Starting {command.upper()}-command for {time.time()}, OS Version: {os_version.major}.{os_version.minor}\n')
     else:
         with open(file_name, 'w') as file:
-            file.write(f'{now} Starting {command.upper()}-command for {time.time()}, OS Version :{os_version.major}.{os_version.minor}')
+            file.write(f'{now} Starting {command.upper()}-command for {time.time()}, OS Version: {os_version.major}.{os_version.minor}')
 
 def system_info():
     """
